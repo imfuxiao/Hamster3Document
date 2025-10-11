6def2faecc899a25f83e7e68f3d331ce287cef77
fu.xiao <im.fu.xiao@gmail.com>
2025-10-11 16:31:42 +0800

    feat: 文档更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import starlight from "@astrojs/starlight";
-import { defineConfig } from "astro/config";
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ihsiao.com",
-  base: "/apps/hamster/v3/docs/",
+  site: 'https://ihsiao.com',
+  base: '/apps/hamster/v3/docs/',
   integrations: [
     starlight({
-      title: "元书输入法",
+      title: '元书输入法',
       social: [
         // {
         //   icon: "github",
@@ -18,25 +18,26 @@ export default defineConfig({
       ],
       sidebar: [
         {
-          label: "简介",
-          slug: "guides/intro",
+          label: '简介',
+          slug: 'guides/intro',
         },
         {
-          label: "键盘皮肤",
+          label: '键盘皮肤',
           items: [
-            { label: "键盘布局", slug: "guides/skins/layout" },
-            { label: "按键样式", slug: "guides/skins/styles" },
-            { label: "事件通知", slug: "guides/skins/notifications" },
-            { label: "按键动画", slug: "guides/skins/animation" },
-            { label: "配置参数", slug: "guides/skins/parameters" },
-            { label: "仓皮肤迁移", slug: "guides/skins/upgrade" },
+            { label: '键盘布局', slug: 'guides/skins/layout' },
+            { label: '按键样式', slug: 'guides/skins/styles' },
+            { label: '按键动作', slug: 'guides/skins/action' },
+            { label: '事件通知', slug: 'guides/skins/notifications' },
+            { label: '按键动画', slug: 'guides/skins/animation' },
+            { label: '配置参数', slug: 'guides/skins/parameters' },
+            { label: '仓皮肤迁移', slug: 'guides/skins/upgrade' },
           ],
         },
         {
-          label: "更新日志",
-          slug: "logs",
+          label: '更新日志',
+          slug: 'logs',
         },
       ],
     }),
   ],
-});
+})
